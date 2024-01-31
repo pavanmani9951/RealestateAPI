bb2c4537b04b0fbc521301f003411f244cfc2aa0
pavan <mani365@gmail.com>
2024-01-31 13:14:26 +0530

    basic commit with no db connection

--- RealEstateAPI/Controllers/CategoriesController.cs
+++ RealEstateAPI/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealEstateAPI.Data;
+using RealEstateAPI.Models;
 
 namespace RealEstateAPI.Controllers
 {
@@ -7,6 +9,65 @@ namespace RealEstateAPI.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-        public List<> categories = new List<T>
+        //public List<Category> categories = new List<Category>
+        //{
+        //    new Category { Id = 1,Name="Villa",ImageUrl=""},
+        //    new Category { Id = 2,Name="Beach villa",ImageUrl=""},
+        //    new Category { Id = 3,Name="Ocean villa",ImageUrl=""}
+
+        //};
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> GetCategories()
+        {
+            return Ok(CategoryStore.categories);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Category> GetCategory(int id)
+        {
+            var category = CategoryStore.categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<Category>> PostCategories([FromBody]Category newCategory)
+        {
+          if (newCategory == null)
+            {
+                return BadRequest();
+            }
+            newCategory.Id = CategoryStore.categories.OrderByDescending(u=>u.Id).FirstOrDefault().Id+1;
+            CategoryStore.categories.Add(newCategory);
+            return Ok(newCategory);
+            
+        }
+        [HttpPut("{id}")]
+        public ActionResult<IEnumerable<Category>>UpdateCategories(int id,[FromBody]Category updateCategory) 
+        {
+            var category = CategoryStore.categories.FirstOrDefault(c=>c.Id == id);
+            if (category == null)
+            {
+                return BadRequest();
+            }
+            category.Id= updateCategory.Id;
+            category.Name= updateCategory.Name;
+            category.ImageUrl= updateCategory.ImageUrl;
+        return Ok(category);
+        }
+        [HttpDelete]
+        public ActionResult<IEnumerable<Category>> DeleteCategories(int id)
+        {
+            var category = CategoryStore.categories.FirstOrDefault(c=>c.Id == id);
+            if (category == null)
+            {
+                return BadRequest();
+            }
+            CategoryStore.categories.Remove(category);
+            return Ok(category);
+        }
     }
 }
---
+++ RealEstateAPI/Data/CategoryStore.cs
@@ -0,0 +1,14 @@
+﻿using RealEstateAPI.Models;
+
+namespace RealEstateAPI.Data
+{
+    public static class CategoryStore
+    {
+        public static List<Category> categories = new List<Category>
+        {
+            new Category{Id=1,Name="ChennaiVillaas",ImageUrl=""},
+            new Category{Id=2,Name="MumbaiVillas",ImageUrl=""},
+            new Category{Id=3,Name="PondiVillas",ImageUrl=""}
+         }; 
+    }
+}
